cc060768ce87c922af4e9eec99b37ab62f258b2e
Lucas Cardinali <lucascardinali92@gmail.com>
2021-08-05 03:19:34 +0100

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GameOff',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
